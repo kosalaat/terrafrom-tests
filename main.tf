ea0f42b1829e80a2beb87e22ab9fc066c407f2ff
Kosala Atapattu <kosala@kosala.net>
2022-08-04 03:25:58 +1200

    updates

--- main.tf
+++ main.tf
@@ -1,21 +1,24 @@
 resource "null_resource" "mynullresource" {
-    triggers = {
-        # plain dicte
-        dict_name = var.dictionaries.name
-        dict_age = var.dictionaries.age
+  triggers = {
+    # plain dictionaries
+    dict_name = var.dictionaries.name
+    dict_age  = var.dictionaries.age
 
-        # list of dicrs
-        join_dic_name = join(",", var.list-of-objects.*.name)
+    # list of dicrs
+    join_dic_name = join(",", var.list_of_objects.*.name)
 
-        # list-of-strings
-        join_list = join(",", var.list-of-strings.*)
+    # list-of-strings
+    join_list = join(",", var.list_of_strings.*)
 
-        # testing boolean
-        name        =  var.boolean ?  var.dictionaries.name : var.list-of-objects[0].name
+    # testing boolean
+    name = var.boolean ? var.dictionaries.name : var.list_of_objects[0].name
 
-        # top level string
-        sample_string = var.string-type
-    }
+    # top level string
+    sample_string = var.string_type
+
+    # nested lists
+    num_from_matrix = var.list_of_lists[1][2]
+  }
 
 }
 
--- variables.tf
+++ variables.tf
@@ -1,30 +1,44 @@
 variable "dictionaries" {
-    type = object({
-        name    = string
-        age     = number
-    })
-    description = "Same as ansible Dict"
+  type = object({
+    name = string
+    age  = number
+  })
+  description = "Same as ansible Dict"
+  default = {
+    age = 1
+    name = "value"
+  }
 }
 
-variable "list-of-strings" {
-    type        = list (string)
-    description = "list of strings"
+variable "list_of_strings" {
+  type        = list(string)
+  description = "list of strings"
 }
 
+variable "list_of_objects" {
+  type = list(object({
+    name = string
+    age  = number
+  }))
 
-variable "list-of-objects" {
-    type = list(object({
-        name    = string
-        age     = number
-    }))
 }
 
 variable "boolean" {
-    type        = bool
-    description = "boolean"
+  type        = bool
+  description = "boolean"
 
 }
 
-variable "string-type" {
-    type        = string 
+variable "string_type" {
+  type = string
+  validation {
+    condition     = (var.string_type == "randomstring2&$%@")
+    error_message = "strings do not match."
+  }
+  default = "randomstring2&$%@"
+}
+
+variable "list_of_lists" {
+  type = list(list(any))
+  default = [ [ 1 ], [1, 2, 3], [3] ]
 }
